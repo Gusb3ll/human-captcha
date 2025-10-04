8106f3dc430d1c5d67d42b5a42d12f16b40de2aa
user.name <kitpipat.0647@gmail.com>
2025-10-05 05:21:43 +0700

    refactor: deps & code

--- apps/server/utils.py
+++ apps/server/utils.py
@@ -1,14 +1,15 @@
-import cv2
 import numpy as np
 from ultralytics import YOLO
 
 _model_cache = {}
 
-def load_model(model_path: str):
-  if (model_path not in _model_cache):
-    _model_cache[model_path] = YOLO(model_path)
 
-  return _model_cache[model_path]
+def load_model(model_path: str):
+    if model_path not in _model_cache:
+        _model_cache[model_path] = YOLO(model_path)
+
+    return _model_cache[model_path]
+
 
 # def predict(image: np.ndarray, model_path: str):
 #   model = load_model(model_path)
@@ -16,7 +17,11 @@ def load_model(model_path: str):
 
 #   return results
 
-def predict_stream(model: YOLO, frame: np.ndarray, ):
-  results = model.predict(frame, conf=0.5, verbose=False)
 
-  return results
+def predict_stream(
+    model: YOLO,
+    frame: np.ndarray,
+):
+    results = model.predict(frame, conf=0.5, verbose=False)
+
+    return results
