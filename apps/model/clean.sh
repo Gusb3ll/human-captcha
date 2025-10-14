ea4c4edad2430370d6c080b61e38a6b1053569ff
user.name <kitpipat.0647@gmail.com>
2025-10-15 05:19:17 +0700

    chore: remove unused

--- apps/model/clean.sh
+++
@@ -1,2 +0,0 @@
-rm -rf ./dataset/**/*.cache
-rm -rf ./run
--- apps/model/main.py
+++ apps/model/main.py
@@ -1,26 +1,18 @@
 import torch
 from ultralytics import YOLO
 
-# Load a pre-trained YOLO model (e.g., YOLOv8n)
 device = "cuda" if torch.cuda.is_available() else "cpu"
-device = "mps" if torch.backends.mps.is_available() else device
 
-model = YOLO("./output/last.pt")
+model = YOLO("./base/yolo11l-pose.pt")
 
-# Train the model
 model.train(
     data="./dataset/dataset.yaml",
-    epochs=150,  # Increase if using early stopping
+    epochs=150,
     imgsz=640,
     batch=16,
     patience=20,
     device=device,
 )
 
-results = model(0, show=True)
+# model(0, show=True)
 model.export(format="onnx")
-
-# for result in results:
-#     print(result.boxes.xyxy)  # x1, y1, x2, y2
-#     print(result.boxes.conf)  # confidence score
-#     print(result.boxes.cls)   # class index
